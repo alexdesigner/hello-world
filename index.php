9f246df6d7a815921f1fe76e196172b2a97f838f
unknown <adm@.(none)>
2010-03-11 20:44:01 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Hello World</title>
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<style> article, aside, figure, footer, header, hgroup, menu, nav, section { display: block; } </style>
+	
+</head>
+<body>
+	<div class="container">
+		<h1>Hello World!</h1>
+		<p>Copyright &copy; <?php echo date('Y') ?> </p>
+	</div>
+</body>
+</html>
